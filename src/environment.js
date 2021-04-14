3f78d3411da2cc9c28e048ff338bd2430f8e86e7
Mansa Samlafo <msamlafo@gmail.com>
2021-04-13 23:44:34 -0400

    minor update

--- src/environment.js
+++ src/environment.js
@@ -2,10 +2,10 @@ let API_URL;
 switch (window.location.hostname){
     case 'localhost':
     case '127.0.0.1':
-        API_URL= 'http://localhost:4000'
-        break
+        API_URL= 'http://localhost:4000';
+        break;
     default: 
-        API_URL= 'https://dateperfectserver.herokuapp.com/'
+        API_URL= 'https://dateperfectserver.herokuapp.com/';
 }
 
 export default API_URL;
